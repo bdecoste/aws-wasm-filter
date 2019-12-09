b87a2d911608541561b41fd980643e0c07496337
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-12-09 16:11:15 -0500

    add boring ssl to inclusion fix

--- toolchain/emcc.sh
+++ toolchain/emcc.sh
@@ -17,6 +17,8 @@ do
         # some zlib headers are treated as system headers
         sed -e 's%[^ ]*/external/zlib/%external/zlib/%' $arg > $arg.tmp
         mv $arg.tmp $arg
+        sed -e 's%[^ ]*/external/boringssl/%external/boringssl/%' $arg > $arg.tmp
+        mv $arg.tmp $arg
         break
     fi
 done
